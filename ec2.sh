b59d56494afc58709149b5a2f12870f264e04d2f
Kelvinskell <kelvinskelll@gmail.com>
2022-07-27 20:14:16 +0100

    feat: Enable Colors

--- ec2.sh
+++ ec2.sh
@@ -3,6 +3,14 @@
 # Enable strict mode
 set eo pipefail
 
+# Define Colours
+Red='\033[1;31m'
+Green='\033[1;32m'
+Blue='\033[1;34m'
+Cyan='\033[1;36m'
+Purple='\033[1;35m'
+NC='\033[0;m'
+
 PS3=""
 echo "Select An EC2 Management Option Below: "
 select option in 'Display Running Instances' 'Start An EC2 Instance' 'Stop An EC2 Instance' 'Terminate An EC2 Instance'
