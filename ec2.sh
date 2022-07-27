499dde72041f6deb1790228c230c92cbd491f059
Kelvinskell <kelvinskelll@gmail.com>
2022-07-27 20:13:19 +0100

    feat: Enable strict mode

--- ec2.sh
+++ ec2.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+# Enable strict mode
+set eo pipefail
+
 PS3=""
-echo "Select An Option Below: "
+echo "Select An EC2 Management Option Below: "
 select option in 'Display Running Instances' 'Start An EC2 Instance' 'Stop An EC2 Instance' 'Terminate An EC2 Instance'
 do
 	echo $option
